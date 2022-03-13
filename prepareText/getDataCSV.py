09267638c48ec4fad1f357171f144b40c74fa2bc
Agnieszka Prokop <agnieszkaprokop@Agnieszkas-MacBook-Pro.local>
2022-03-13 09:55:06 +0100

    usunięcie niepotrzebnego kodu

--- prepareText/getDataCSV.py
+++ prepareText/getDataCSV.py
@@ -4,14 +4,4 @@ import pandas as pd
 
 def get_data_csv(path: str) -> str:
     df = pd.read_csv(path, skipinitialspace=True, usecols=['title'])
-    # rows = []
-    # with open(path, 'r') as file:
-    #     csvreader = csv.reader(file)
-    #     # header = next(csvreader)
-    #     for row in csvreader:
-    #         rows.append(row['title'])
-    # s = " "
-    # str_result = ""
-    # for x in rows:
-    #     str_result += s.join(x)
     return df.to_string()
